9df8e530ace98c2062b088c3152344c2b5c73530
thanabodee <thanabodee2661@gmail.com>
2024-10-21 11:08:29 +0700

    add more button number

--- src/app/pages/rooms/[roomId]/page.tsx
+++ src/app/pages/rooms/[roomId]/page.tsx
@@ -213,6 +213,27 @@ export default function Rooms({ params }: { params: { roomId: string } }) {
       >
         5
       </button>
+      <button
+        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+        type="button"
+        onClick={() => handleVote("6")}
+      >
+        6
+      </button>
+      <button
+        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+        type="button"
+        onClick={() => handleVote("7")}
+      >
+        7
+      </button>
+      <button
+        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+        type="button"
+        onClick={() => handleVote("8")}
+      >
+        8
+      </button>
 
       <ul>
         {users?.map((user) => (
