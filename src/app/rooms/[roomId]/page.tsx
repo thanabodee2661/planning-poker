acc1b50121f30b2d252bce9fc82633815e44eb24
thanabodee <thanabodee2661@gmail.com>
2024-10-22 08:35:26 +0700

    add css to room page

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -28,8 +28,6 @@ export default function Rooms({ params }: { params: { roomId: string } }) {
   const name = searchParams.get("name");
 
   useEffect(() => {
-    console.log(name);
-
     if (name) {
       const uuid = v7();
       setDetail({ id: uuid, name: name } as UserDetail);
@@ -78,7 +76,7 @@ export default function Rooms({ params }: { params: { roomId: string } }) {
           vote: "",
         }))
       );
-      setResultVote(undefined)
+      setResultVote(undefined);
     });
 
     socket.on("showVoteResult", () => {
@@ -86,8 +84,6 @@ export default function Rooms({ params }: { params: { roomId: string } }) {
     });
 
     socket.on("summaryVoteResult", (resultVote) => {
-      console.log("summaryVoteResult", resultVote);
-
       setResultVote(resultVote);
     });
 
@@ -100,7 +96,6 @@ export default function Rooms({ params }: { params: { roomId: string } }) {
 
   useEffect(() => {
     if (isShowVote) {
-      console.log('summary active');
       summarize();
     }
   }, [users]);
@@ -137,8 +132,6 @@ export default function Rooms({ params }: { params: { roomId: string } }) {
   };
 
   const summarize = async () => {
-    console.log(users);
-
     const resultVote = await groupBy(users, (user) => user.vote);
     socket?.emit("summaryVote", {
       roomId: 1,
@@ -148,118 +141,142 @@ export default function Rooms({ params }: { params: { roomId: string } }) {
   };
 
   return (
-    <div>
-      <h2>Room ID: {params.roomId}</h2>
-      <div className="w-96">
-        <div className="relative w-full min-w-[200px]">
-          <textarea
-            className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
-            placeholder=" "
-            value={message}
-            onChange={handleMessage}
-          ></textarea>
-          <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-            Message
+    <div className="grid grid-cols-1 mt-10 mx-10 gap-4 lg:grid-cols-2">
+      <div className="justify-self-center lg:justify-self-end">
+        <div className="grid grid-cols-1 justify-self-center w-96 mb-2">
+          <label className="block mb-2 text-md text-slate-600">
+            Room ID: {params.roomId}
           </label>
+
+          <div className="relative w-full">
+            <textarea
+              className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-white border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-white outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-white focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+              placeholder=" "
+              value={message}
+              onChange={handleMessage}
+            ></textarea>
+            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[12px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-white before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-white after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-white peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-white peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              Message
+            </label>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 w-96 justify-self-center mb-2">
+          <button
+            className="rounded-md bg-green-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+            onClick={handleShowVote}
+          >
+            Show Vote
+          </button>
+          <button
+            className="rounded-md bg-amber-600 py-2 px-4 border border-transparent text-center text-sm text-slate-800 transition-all shadow-md hover:shadow-lg focus:bg-amber-700 focus:shadow-none active:bg-amber-700 hover:bg-amber-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+            onClick={handleClear}
+          >
+            Clear
+          </button>
+        </div>
+
+        <div className="grid grid-cols-1 justify-self-center w-96 mb-2">
+          <label className="block mb-2 text-sm text-slate-600">Voting</label>
+          <div className="grid grid-cols-8 gap-2">
+            <button
+              className="rounded-md bg-blue-600 w-10 h-10 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              onClick={() => handleVote("1")}
+            >
+              1
+            </button>
+            <button
+              className="rounded-md bg-blue-600 w-10 h-10 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              onClick={() => handleVote("2")}
+            >
+              2
+            </button>
+            <button
+              className="rounded-md bg-blue-600 w-10 h-10 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              onClick={() => handleVote("3")}
+            >
+              3
+            </button>
+            <button
+              className="rounded-md bg-blue-600 w-10 h-10 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              onClick={() => handleVote("4")}
+            >
+              4
+            </button>
+            <button
+              className="rounded-md bg-blue-600 w-10 h-10 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              onClick={() => handleVote("5")}
+            >
+              5
+            </button>
+            <button
+              className="rounded-md bg-blue-600 w-10 h-10 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              onClick={() => handleVote("6")}
+            >
+              6
+            </button>
+            <button
+              className="rounded-md bg-blue-600 w-10 h-10 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              onClick={() => handleVote("7")}
+            >
+              7
+            </button>
+            <button
+              className="rounded-md bg-blue-600 w-10 h-10 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              onClick={() => handleVote("8")}
+            >
+              8
+            </button>
+          </div>
+
+          <hr className="mt-4" />
+        </div>
+        <div className="grid grid-cols-2 w-96 justify-self-center">
+          <div>
+            <ul>
+              {users?.map((user) => (
+                <li key={user?.id}>
+                  {user?.name} vote:{" "}
+                  {user.id === detail?.id || !user?.vote || isShowVote
+                    ? user?.vote
+                    : "*"}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-      <button
-        className="rounded-md bg-green-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={handleShowVote}
-      >
-        Show Vote
-      </button>
-      <button
-        className="rounded-md bg-amber-600 py-2 px-4 border border-transparent text-center text-sm text-slate-800 transition-all shadow-md hover:shadow-lg focus:bg-amber-700 focus:shadow-none active:bg-amber-700 hover:bg-amber-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={handleClear}
-      >
-        Clear
-      </button>
-      <p>Voting</p>
-      <button
-        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={() => handleVote("1")}
-      >
-        1
-      </button>
-      <button
-        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={() => handleVote("2")}
-      >
-        2
-      </button>
-      <button
-        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={() => handleVote("3")}
-      >
-        3
-      </button>
-      <button
-        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={() => handleVote("4")}
-      >
-        4
-      </button>
-      <button
-        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={() => handleVote("5")}
-      >
-        5
-      </button>
-      <button
-        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={() => handleVote("6")}
-      >
-        6
-      </button>
-      <button
-        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={() => handleVote("7")}
-      >
-        7
-      </button>
-      <button
-        className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
-        type="button"
-        onClick={() => handleVote("8")}
-      >
-        8
-      </button>
 
-      <ul>
-        {users?.map((user) => (
-          <li key={user?.id}>
-            {user?.name} vote:{" "}
-            {user.id === detail?.id || !user?.vote || isShowVote
-              ? user?.vote
-              : "*"}
-          </li>
-        ))}
-      </ul>
-
-      {resultVote ? (
-        <div>
-          <h2>Result Vote</h2>
-          <ul>
-            {Object.entries(resultVote).map(([key, value]) => (
-              <li key={key}>
-                vote: {key ? key : "ไม่มี vote"} = {value.length}
-              </li>
-            ))}
-          </ul>
+      <div className="justify-self-center lg:justify-self-start">
+        <div className="grid grid-cols-1 w-96 justify-self-center">
+          {resultVote ? (
+            <div className="grid grid-flow-row">
+              <label className="block mb-2 text-md text-slate-600">
+                Result Vote
+              </label>
+              <ul className="border border-white rounded-md p-2">
+                {Object.entries(resultVote).map(([key, value]) => (
+                  <li key={key}>
+                    vote: {key ? key : "ไม่มี vote"} = {value.length}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
-      ) : (
-        ""
-      )}
+      </div>
     </div>
   );
 }
